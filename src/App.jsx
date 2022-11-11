05110e0b5b1099ce890857a359832973872d29e0
davidmuir1999 <105323893+davidmuir1999@users.noreply.github.com>
2022-11-11 14:42:21 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+import Title from './components/Title.jsx'
+import Modal from './components/modal.jsx'
+
+function App() {
+  return (
+    <>
+      <Title />
+      <div className='todo__wrapper'>
+        <Todo />
+        <Todo />
+        <Todo />
+      </div>
+      <Modal />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import './Todo.css'
+
+function Todo() {
+    return (
+        <div className='todo'>
+            <h2>Finish Crash Course</h2>
+            <button>Delete</button>
+        </div>
+    )
+}
+
+export default Todo
